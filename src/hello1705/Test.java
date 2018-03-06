8364112d283c56037ab9b15b9cefa8f1b253c179
jyx <15165194064@163.com>
2018-03-06 16:15:58 +0800

    Revert: 123

--- src/hello1705/Test.java
+++ src/hello1705/Test.java
@@ -2,7 +2,7 @@ package hello1705;
 
 public class Test {
     public static void main(String[] args){
-    	String a = "1705githubtest123";
+    	String a = "upd123ateidea";
         System.out.println(a);
     }
 }
