c5e30078765c69877c70c5da60f1ce8690d32d00
jyx <15165194064@163.com>
2018-03-06 16:20:39 +0800

    Update Test.java

--- src/hello1705/Test.java
+++ src/hello1705/Test.java
@@ -2,7 +2,7 @@ package hello1705;
 
 public class Test {
     public static void main(String[] args){
-    	String a = "1705githubtest123";
+    	String a = "web";
         System.out.println(a);
     }
 }
